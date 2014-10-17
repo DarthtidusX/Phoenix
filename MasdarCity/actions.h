0ee467678e4be903a703897aae9eb84404f94ddb
DarthtidusX <darthtidus@googlemail.com>
2014-10-17 21:09:57 +0200

    Update actions.h
    updated

--- MasdarCity/actions.h
+++ MasdarCity/actions.h
@@ -1,5 +1,30 @@
-﻿#ifndef ACTIONS_H
+#ifndef ACTIONS_H
 #define ACTIONS_H
+
+/*************** DIREKTIVEN ***************/
+#define MOTOR_LINKS 1                    // Motor linkes Rad, Port 1
+#define MOTOR_RECHTS 2                   // Motor rechtes Rad, Port2
+#define MOTOR_RICHTUNG_VORWAERTS 1       // Zahlen können umgekehrt sein
+#define MOTOR_RICHTUNG_RUECKWAERTS 0
+#define SPEED 10                         // Standardgeschwindigkeit
+#define STOP 0                           // Geschwindigkeit auf 0 setzen
+#define Startpoint_A 64                  // Startpunkt A ist der 64.te Punkt auf dem Gitternetz
+#define Startpoint_B 68                  // Startpunkt B ist der 68.te Punkt auf dem Gitternetz
+/*************** DIREKTIVEN ***************/
+
+
+/****** GITTERNETZ ******/           // Wobei die Ecken 0, 6, 63, 69 nicht beruecksichtigt werden
+/*  0  1  2  3  4  5  6 */
+/*  7  8  9 10 11 12 13 */
+/* 14 15 16 17 18 19 20 */
+/* 21 22 23 24 25 26 27 */
+/* 28 29 30 31 32 33 34 */
+/* 35 36 37 38 39 40 41 */
+/* 42 43 44 45 46 47 48 */
+/* 49 50 51 52 53 54 55 */
+/* 56 57 58 59 60 61 62 */
+/* 63  A 65 66 67  B 69 */
+/****** GITTERNETZ ******/
 
 /*************** FUNKTIONSDEKLARATIONEN ****************/
 // Fahrplan
@@ -27,4 +52,4 @@ void fetch_guest();
 // Suchverfahren
 void breitensuche(byte start);
 /*************** FUNKTIONSDEKLARATIONEN ****************/
-
+#endif
